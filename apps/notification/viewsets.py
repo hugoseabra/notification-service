ca3747c34394a859151b19f7a944cb0334972f4b
Marco Silva <sodmlx@gmail.com>
2020-06-24 16:05:40 -0300

    Funcionalidade Viewset Subscriber #2005

--- apps/notification/viewsets.py
+++ apps/notification/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
 
-from .serializers import NamespaceSerializer, GroupSerializer
+from .serializers import NamespaceSerializer, GroupSerializer, SubscriberSerializer
 
 
 class NamespaceViewSet(ModelViewSet):
@@ -10,3 +10,7 @@ class NamespaceViewSet(ModelViewSet):
 class GroupViewSet(ModelViewSet):
     serializer_class = GroupSerializer
     queryset = GroupSerializer.Meta.model.objects.get_queryset()
+
+class SubscriberViewSet(ModelViewSet):
+    serializer_class = SubscriberSerializer
+    queryset = SubscriberSerializer.Meta.model.objects.get_queryset()
