7ac78ca44b7800b63119404644862c99c64508e2
Marco Silva <sodmlx@gmail.com>
2020-06-24 14:33:20 -0300

    Funcionalidade Endpoint Group #2001

--- apps/notification/admin.py
+++ apps/notification/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Namespace
+from .models import Namespace, Group
 
+from .forms import NamespaceForm, GroupForm
 
 @admin.register(Namespace)
 class NamespaceAdmin(admin.ModelAdmin):
@@ -14,6 +15,23 @@ class NamespaceAdmin(admin.ModelAdmin):
         'external_id',
         'description',
     )
+    form = NamespaceForm
     list_filter = ('active', 'created_at', 'updated_at')
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = (
+        'active',
+        'created_at',
+        'updated_at',
+        'uuid',
+        'name',
+        'alias',
+        'namespace',
+    )
+    form = GroupForm
+    list_filter = ('active', 'created_at', 'updated_at', 'namespace')
     search_fields = ('name',)
     date_hierarchy = 'created_at'
