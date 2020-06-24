028a69052b94fd3351c2baf16d4cb2608906b9c9
Marco Silva <sodmlx@gmail.com>
2020-06-24 13:56:06 -0300

    Funcionalidade Viewset Group #2000

--- apps/notification/viewsets.py
+++ apps/notification/viewsets.py
@@ -1,8 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
-from .serializers import NamespaceSerializer
+from .serializers import NamespaceSerializer, GroupSerializer
 
 
 class NamespaceViewSet(ModelViewSet):
     serializer_class = NamespaceSerializer
     queryset = NamespaceSerializer.Meta.model.objects.get_queryset()
+
+class GroupViewSet(ModelViewSet):
+    serializer_class = GroupSerializer
+    queryset = GroupSerializer.Meta.model.objects.get_queryset()
