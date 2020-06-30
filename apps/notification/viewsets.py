f701bbdd714bb2794eae68e68f8286d885c1c20d
Marco Barreto <sodmlx@gmail.com>
2020-06-30 18:17:49 -0300

    Criaçao do Endpoint Device #2011

--- apps/notification/viewsets.py
+++ apps/notification/viewsets.py
@@ -1,11 +1,17 @@
 from rest_framework import status, permissions
+
 from rest_framework.response import Response
+
 from rest_framework.serializers import ListSerializer
+
 from rest_framework.viewsets import ModelViewSet
 
 from core.util.uuid import get_validated_uuid_from_string
+
 from core.viewsets import FieldRequestViewsetMixin
+
 from . import serializers
+
 from django.utils.translation import gettext_lazy as _
 
 from .models import Namespace
@@ -94,51 +100,11 @@ class SubscriberViewSet(FieldRequestViewsetMixin, ModelViewSet):
 
         return Response(serializer.data)
 
-
-class DeviceViewSet(ModelViewSet):
+class DeviceViewSet(FieldRequestViewsetMixin, ModelViewSet):
     serializer_class = serializers.DeviceSerializer
     queryset = serializers.DeviceSerializer.Meta.model.objects.get_queryset()
 
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def get_serializer(self, *args, **kwargs):
-        serializer = super().get_serializer(*args, **kwargs)
-
-        if isinstance(serializer, ListSerializer) is False:
-            subscriber_pk = get_validated_uuid_from_string(
-                self.kwargs.get('subscriber_pk')
-            )
-            serializer.subscriber_pk = subscriber_pk
-
-        return serializer
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-
-        subscriber_pk = get_validated_uuid_from_string(
-            self.kwargs.get('subscriber_pk')
-        )
-
-        if subscriber_pk:
-            queryset = queryset.filter(subscriber_id=subscriber_pk)
-        else:
-            queryset = queryset.none()
-
-        return queryset
-
-    def create(self, request, *args, **kwargs):
-        subscriber_pk = get_validated_uuid_from_string(
-            self.kwargs.get('project_pk')
-        )
-        data = request.data.copy()
-        data.update({'subscriber': device_pk})
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(data)
-        return Response(serializer.data,
-                        status=status.HTTP_201_CREATED,
-                        headers=headers)
 
 class NotificationViewSet(ModelViewSet):
     serializer_class = serializers.NotificationSerializer
