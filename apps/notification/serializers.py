a4ce33ad7b45a17b571df69bd9f58a5e2090c438
Marco Barreto <sodmlx@gmail.com>
2020-07-01 21:39:56 -0300

    Funcionalidade Serializer Device #2009

--- apps/notification/serializers.py
+++ apps/notification/serializers.py
@@ -131,10 +131,33 @@ class DeviceSerializer(FormSerializerMixin, serializers.ModelSerializer):
         fields = (
             'pk',
             'name',
+            'broker_id',
+            'device_type',
+            'model',
+            'unique_id',
+            'brand',
+            'os_build_number',
+            'os_version',
+            'os_bundle_id',
+            'os_readable_version',
+            'android_fringerprint',
+            'android_install_time',
+            'android_bootloader',
+            'ios_device_token',
+            'subscriber',
             'active',
             'created_at',
             'updated_at',
         )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.subscribe_pk = None
+
+    def get_form(self, data=None, files=None, **kwargs):
+        if data:
+            data.update({'subscribe': self.subscribe_pk})
+        return super().get_form(data, files, **kwargs)
 
 
 class NotificationSerializer(FormSerializerMixin, serializers.ModelSerializer):
